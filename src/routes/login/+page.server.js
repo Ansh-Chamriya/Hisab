bb76701a579e28735b47e2f75445fcf3d0305eda
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-03-19 18:25:40 +0530

    login route changed

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -5,7 +5,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async (event) => {
 	const session = await event.locals.getSession();
-
+	console.log(session);
 	if (session) {
 		throw redirect(301, '/dashboard');
 	}
