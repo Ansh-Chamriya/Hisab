5840b91c80f924c3c7f212ff48e4e2037b85b62f
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-03-20 22:56:59 +0530

    login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -9,7 +9,7 @@
 		const { data, error } = await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
-				redirectTo: `http://hisab-seven.vercel.app/auth/callback`,
+				redirectTo: `http://hisab-seven.vercel.app/login/callback`,
 				queryParams: {
 					apikey: PUBLIC_DATABASE_ANON_KEY
 				}
