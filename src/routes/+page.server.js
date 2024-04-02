8fca4a7179434559c74d6d863416c7f62761b231
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-04-02 17:07:28 +0530

    Nice

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,11 @@
 import supabase from '$lib/supabaseClient';
 import { redirect } from '@sveltejs/kit';
 
-// const {
-// 	data: { session }
-// } = await supabase.auth.getSession();
-// if (session) {
-// 	throw redirect(301, '/dashboard');
-// }
+export async function load() {
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+	if (session) {
+		throw redirect(301, '/dashboard');
+	}
+}
