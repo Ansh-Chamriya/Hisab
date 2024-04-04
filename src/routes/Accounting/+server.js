6ac67312a105b4b25d17d67fe90f6e4ec108621b
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-04-05 00:40:29 +0530

    a2a2a

--- src/routes/Accounting/+server.js
+++ src/routes/Accounting/+server.js
@@ -3,22 +3,21 @@ import supabaseClient from '$lib/supabaseClient.js';
 import supabase from '$lib/supabaseClient.js';
 export async function POST({ request }) {
 	const { start, end, userid } = await request.json();
-	const userSession = await request.locals.getsession();
 	const { data: calenderIdata, error } = await supabase
 		.from('income')
 		.select('article,price,qty,time,date')
-		.eq('user_id', userSession.user.id)
+		.eq('user_id', userid)
 		.gte('date', start)
 		.lte('date', end);
 	const { data: calenderEdata, error: e } = await supabase
 		.from('expense')
 		.select('article,price,qty,time,date')
-		.eq('user_id', userSession.user.id)
+		.eq('user_id', userid)
 		.gte('date', start)
 		.lte('date', end);
 	console.log(calenderIdata, calenderEdata);
 	console.log(error, e);
-	console.log('userId', userSession.user.id);
+	console.log('userId', userid);
 
 	return json({
 		AccCalenderIdata: calenderIdata,
