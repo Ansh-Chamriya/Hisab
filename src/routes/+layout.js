e36e7141850c0c6b1d1fc2fd71af46c05c8aec44
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-04-06 20:52:36 +0530

    added vercel performance script

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,7 +2,9 @@ import { PUBLIC_DATABASE_ANON_KEY, PUBLIC_DATABASE_URL } from '$env/static/publi
 import { createBrowserClient, isBrowser, parse } from '@supabase/ssr';
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+injectSpeedInsights();
 inject({ mode: dev ? 'development' : 'production' });
 export const load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
