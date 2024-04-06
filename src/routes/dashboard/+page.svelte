4d5a536ff8888f771cde45e44a116523a7a1c8c0
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-04-06 17:53:49 +0530

    still adjusting

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -136,7 +136,7 @@
 		</div>
 	</nav>
 
-	<div class="filter-container mt-4 flex w-fit flex-row justify-evenly">
+	<div class="filter-container mt-4 flex w-fit flex-row justify-evenly gap-1">
 		<Button
 			variant={active_tab === 'today' ? 'default' : 'secondary'}
 			on:click={() => (active_tab = 'today')}
