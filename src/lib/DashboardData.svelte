dacfc1d34ded0e6d7cc55ed79ff11f1671b5414d
Ansh Chamriya <AnshChamariya922@gmail.com>
2024-04-10 15:56:23 +0530

    testing

--- src/lib/DashboardData.svelte
+++ src/lib/DashboardData.svelte
@@ -23,84 +23,86 @@
 <div class=" mt-4 font-bold text-slate-600">
 	<span class="mb-6 ml-5">Today, {date}</span>
 	{#each Datas as Data}
-		<Dialog.Root>
-			<Dialog.Trigger>
-				<div
-					class=" relative m-auto mt-2 flex h-16 w-[85vw] rounded-lg border border-slate-200 text-black"
-				>
-					<Avatar.Root class="absolute left-2 top-2">
-						<Avatar.Image src="#" alt="userImage" />
-
-						<Avatar.Fallback>{Data.article.slice(0, 1)}</Avatar.Fallback>
-					</Avatar.Root>
-					<h2 class="absolute left-14 top-1">{Data.article}</h2>
-					<h2 class="absolute right-5 top-1">Qty: {Data.qty}</h2>
-					<span class="absolute bottom-2 right-5"
-						>{Data.price.toLocaleString('en-IN', {
-							maximumFractionDigits: 2,
-							style: 'currency',
-							currency: 'INR'
-						})}
-					</span>
-					<span class="absolute bottom-2 left-14 text-sm text-slate-400">{Data.time}</span>
-					<span class="absolute bottom-2 left-[7.25rem] text-sm text-slate-400"
-						>{dayjs(Data.date).format('DD/MM/YYYY')}</span
+		<div class="m-4">
+			<Dialog.Root>
+				<Dialog.Trigger>
+					<div
+						class=" relative m-auto mt-2 flex h-16 w-[85vw] rounded-lg border border-slate-200 text-black"
 					>
-				</div></Dialog.Trigger
-			>
-			<Dialog.Content class="m-4 sm:max-w-[425px]">
-				<Dialog.Header>
-					<Dialog.Title>Edit Income</Dialog.Title>
-					<Dialog.Description>
-						Make changes to the Income here. Click save when you're done.
-					</Dialog.Description>
-				</Dialog.Header>
-				<div class="grid gap-4 py-4">
-					<form action="?/update" method="post">
-						<div class="mb-4 grid grid-cols-4 items-center gap-4">
-							<Label for="articlePrice" class="text-right">Price</Label>
-							<input type="hidden" name="incomeId" value={Data.income_id} />
-							<Select.Root>
-								<Select.Trigger class="w-full bg-slate-50" name="updatePrice" id="articlePrice">
-									<Select.Value />
-								</Select.Trigger>
-								<Select.Content class="bg-slate-50">
-									<Select.Group>
-										<Select.Label>Price</Select.Label>
-										{#if Data.article == 'Idli-khiru'}
-											{#each idliPrices as idliPrice}
-												<Select.Item value={idliPrice}>{idliPrice}</Select.Item>
-											{/each}
-										{:else if Data.article == 'Chutney'}
-											{#each chutneyPrices as chutneyPrice}
-												<Select.Item value={chutneyPrice}>{chutneyPrice}</Select.Item>
-											{/each}
-										{:else if Data.article == 'Menduvada-khiru'}
-											{#each MenduvadaPrices as MenduvadaPrice}
-												<Select.Item value={MenduvadaPrice}>{MenduvadaPrice}</Select.Item>
-											{/each}
-										{/if}
-									</Select.Group>
-								</Select.Content>
-								<Select.Input name="updatePrice" />
-							</Select.Root>
-						</div>
-						<div class="mb-4 grid grid-cols-4 items-center gap-4">
-							<Label for="articleQty" class="text-right">Quantity</Label>
-							<Input
-								id="articleQty"
-								name="articleQty"
-								type="number"
-								value={Data.qty}
-								class="col-span-3"
-							/>
-						</div>
-						<Dialog.Footer>
-							<Button type="submit">Save changes</Button>
-						</Dialog.Footer>
-					</form>
-				</div>
-			</Dialog.Content>
-		</Dialog.Root>
+						<Avatar.Root class="absolute left-2 top-2">
+							<Avatar.Image src="#" alt="userImage" />
+
+							<Avatar.Fallback>{Data.article.slice(0, 1)}</Avatar.Fallback>
+						</Avatar.Root>
+						<h2 class="absolute left-14 top-1">{Data.article}</h2>
+						<h2 class="absolute right-5 top-1">Qty: {Data.qty}</h2>
+						<span class="absolute bottom-2 right-5"
+							>{Data.price.toLocaleString('en-IN', {
+								maximumFractionDigits: 2,
+								style: 'currency',
+								currency: 'INR'
+							})}
+						</span>
+						<span class="absolute bottom-2 left-14 text-sm text-slate-400">{Data.time}</span>
+						<span class="absolute bottom-2 left-[7.25rem] text-sm text-slate-400"
+							>{dayjs(Data.date).format('DD/MM/YYYY')}</span
+						>
+					</div></Dialog.Trigger
+				>
+				<Dialog.Content class="m-4 sm:max-w-[425px]">
+					<Dialog.Header>
+						<Dialog.Title>Edit Income</Dialog.Title>
+						<Dialog.Description>
+							Make changes to the Income here. Click save when you're done.
+						</Dialog.Description>
+					</Dialog.Header>
+					<div class="grid gap-4 py-4">
+						<form action="?/update" method="post">
+							<div class="mb-4 grid grid-cols-4 items-center gap-4">
+								<Label for="articlePrice" class="text-right">Price</Label>
+								<input type="hidden" name="incomeId" value={Data.income_id} />
+								<Select.Root>
+									<Select.Trigger class="w-full bg-slate-50" name="updatePrice" id="articlePrice">
+										<Select.Value />
+									</Select.Trigger>
+									<Select.Content class="bg-slate-50">
+										<Select.Group>
+											<Select.Label>Price</Select.Label>
+											{#if Data.article == 'Idli-khiru'}
+												{#each idliPrices as idliPrice}
+													<Select.Item value={idliPrice}>{idliPrice}</Select.Item>
+												{/each}
+											{:else if Data.article == 'Chutney'}
+												{#each chutneyPrices as chutneyPrice}
+													<Select.Item value={chutneyPrice}>{chutneyPrice}</Select.Item>
+												{/each}
+											{:else if Data.article == 'Menduvada-khiru'}
+												{#each MenduvadaPrices as MenduvadaPrice}
+													<Select.Item value={MenduvadaPrice}>{MenduvadaPrice}</Select.Item>
+												{/each}
+											{/if}
+										</Select.Group>
+									</Select.Content>
+									<Select.Input name="updatePrice" />
+								</Select.Root>
+							</div>
+							<div class="mb-4 grid grid-cols-4 items-center gap-4">
+								<Label for="articleQty" class="text-right">Quantity</Label>
+								<Input
+									id="articleQty"
+									name="articleQty"
+									type="number"
+									value={Data.qty}
+									class="col-span-3"
+								/>
+							</div>
+							<Dialog.Footer>
+								<Button type="submit">Save changes</Button>
+							</Dialog.Footer>
+						</form>
+					</div>
+				</Dialog.Content>
+			</Dialog.Root>
+		</div>
 	{/each}
 </div>
